f9ee4acdbc3e55673a95df9bf64c1655e5cc16a3
Jose1102 <69309340+Jose1102@users.noreply.github.com>
2025-02-06 10:52:41 -0500

    Se genera proyecto base
    se genera proyecto base de react

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+// src/components/MiComponente.js
+
+
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import React from 'react';
+
+// Definimos un componente funcional
+const Login = () => {
+  return (
+    <div>
+      <Button variant="text">Text</Button>
+    </div>
+  );
+};
+
+export default Login;
